bc61df3d6e23ae621b8705413650976ad86cab81
CalielOliveira <62117722+CalielOliveira@users.noreply.github.com>
2021-06-08 13:27:33 -0300

    creating a Sudoku class and changing functions to methods

--- sudoku.py
+++ sudoku.py
@@ -1,89 +1,98 @@
 from math import ceil
 from collections import Counter
 
-all_possible_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-"""sudoku_camp = [
-    [8, 0, 0, 1, 0, 3, 0, 4, 0],
-    [0, 3, 0, 0, 0, 0, 0, 0, 7],
-    [0, 0, 0, 4, 0, 7, 3, 0, 0],
-    [3, 0, 6, 0, 1, 2, 8, 0, 4],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [7, 0, 5, 8, 4, 0, 6, 0, 3],
-    [0, 0, 8, 6, 0, 1, 0, 0, 0],
-    [2, 0, 0, 0, 0, 0, 0, 3, 0],
-    [0, 6, 0, 2, 0, 5, 0, 0, 8]
-]"""
+"""Sudoku example:
+list:   [[8, 0, 0, 1, 0, 3, 0, 4, 0],
+        [0, 3, 0, 0, 0, 0, 0, 0, 7],
+        [0, 0, 0, 4, 0, 7, 3, 0, 0],
+        [3, 0, 6, 0, 1, 2, 8, 0, 4],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [7, 0, 5, 8, 4, 0, 6, 0, 3],
+        [0, 0, 8, 6, 0, 1, 0, 0, 0],
+        [2, 0, 0, 0, 0, 0, 0, 3, 0],
+        [0, 6, 0, 2, 0, 5, 0, 0, 8]]
+        
+text: 070060000000000006041708090003070400100205008002030600030602800900000507000080000
+"""
 
 
-def text_to_camp(text):
-    local_sudoku = [[0]*9]*9
-    n = [st for st in str(text)]
-    start, end = 0, 9
-    for row in range(9):
-        local_sudoku[row] = [int(x) for x in n[start:end]]
-        start += 9
-        end += 9
-    return local_sudoku
+class Sudoku:
+    camp = [[0] * 9] * 9
+    solved = False
 
+    def __init__(self, sudoku_numbers: str):
+        self.sudoku_numbers = sudoku_numbers
+        n = [st for st in str(sudoku_numbers)]
+        start, end = 0, 9
+        for row in range(9):
+            self.camp[row] = [int(x) for x in n[start:end]]
+            start += 9
+            end += 9
 
-def get_possibilities(x, y, local_sudoku_camp):
-    counter_numbers = Counter(all_possible_numbers)
-    row_possibilities = counter_numbers - Counter(local_sudoku_camp[x])
-    column_possibilities = counter_numbers - Counter([row[y] for row in local_sudoku_camp])
-    maximum_x, maximum_y = ceil((x+1)/3) * 3, ceil((y+1)/3) * 3
-    counter_square = Counter([])
+    def __str__(self):
+        text = ""
+        for row in self.camp:
+            for number in row:
+                text += str(number) + "  "
+            text += '\n'
+        return text
 
-    # line too long for comprehension
-    for column in range(maximum_y-3, maximum_y):
-        for row in range(maximum_x-3, maximum_x):
-            counter_square.update([local_sudoku_camp[row][column]])
+    def get_possibilities(self, x, y):
+        counter_numbers = Counter([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        row_possibilities = counter_numbers - Counter(self.camp[x])
+        column_possibilities = counter_numbers - Counter([row[y] for row in self.camp])
+        maximum_x, maximum_y = ceil((x + 1) / 3) * 3, ceil((y + 1) / 3) * 3
+        counter_square = Counter([])
 
-    square_possibilities = counter_numbers - counter_square
-    total_possibilities = row_possibilities & column_possibilities & square_possibilities
-    return total_possibilities
+        for column in range(maximum_y - 3, maximum_y):
+            for row in range(maximum_x - 3, maximum_x):
+                counter_square.update([self.camp[row][column]])
 
+        square_possibilities = counter_numbers - counter_square
+        total_possibilities = row_possibilities & column_possibilities & square_possibilities
+        return total_possibilities
 
-def set_numbers(sudoku_camp, x=0, y=0):
-    if sudoku_camp[x][y]:
-        next_number = get_next_zero(sudoku_camp)
-        if next_number:
-            return set_numbers(sudoku_camp, next_number[0], next_number[1])
+    def set_numbers(self, x, y):
+        if self.camp[x][y]:
+            next_number = self.get_next_zero()
+            if next_number:
+                return self.set_numbers(next_number[0], next_number[1])
+            else:
+                return True
         else:
-            return True
-    else:
-        possibilities = list(get_possibilities(x, y, sudoku_camp))
-        if len(possibilities) >= 1:
-            for poss in possibilities:
-                sudoku_camp[x][y] = poss
-                is_possible = False
-                next_number = get_next_zero(sudoku_camp)
-                if next_number:
-                    is_possible = set_numbers(sudoku_camp, next_number[0], next_number[1])
-                else:
-                    return True
-                if is_possible:
-                    return True
-            sudoku_camp[x][y] = 0
-            return False
+            possibilities = list(self.get_possibilities(x, y))
+            if len(possibilities) >= 1:
+                for poss in possibilities:
+                    self.camp[x][y] = poss
+                    next_number = self.get_next_zero()
+                    if next_number:
+                        is_possible = self.set_numbers(next_number[0], next_number[1])
+                    else:
+                        return True
+                    if is_possible:
+                        return True
+                self.camp[x][y] = 0
+                return False
 
+            else:
+                return False
+
+    def get_next_zero(self):
+        for row in range(9):
+            for column in range(9):
+                if not self.camp[row][column]:
+                    return row, column
+        return False
+
+    def solve(self):
+        if self.set_numbers(0, 0):
+            print("Sudoku solved")
+            self.solved = True
         else:
-            return False
+            print("Impossible to solve")
 
 
-def get_next_zero(local_sudoku_camp):
-    for row in range(9):
-        for column in range(9):
-            if not local_sudoku_camp[row][column]:
-                return row, column
-    return False
-
-
-def show_camp(camp):
-    for row in camp:
-        print(row)
-
-
-sudoku = text_to_camp("070060000000000006041708090003070400100205008002030600030602800900000507000080000")
-set_numbers(sudoku)
-show_camp(sudoku)
-# show_camp(text_to_camp("070060000000000006041708090003070400100205008002030600030602800900000507000080000"))
+if __name__ == "__main__":
+    sudoku = Sudoku("070060000000000006041708090003070400100205008002030600030602800900000507000080000")
+    sudoku.solve()
+    print(sudoku)
